cca379d081b30e4587f99bd6344b4bb0afbb3fae
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-11-05 12:10:06 +0300

    add test, getDaysInMonth, isWeekEnd

--- test/datetime/TestDateTime.ts
+++ test/datetime/TestDateTime.ts
@@ -169,5 +169,103 @@ contract("---------- Test isWeekDay ---------- BokkyPooBahsDateTimeContract", as
   });
 });
 
+contract("---------- Test isWeekEnd ---------- BokkyPooBahsDateTimeContract", async ([_, owner, wallet1, wallet2, wallet3, wallet4, wallet5]) => {
+  let testDateTime: TestDateTimeInstance;
 
+  beforeEach(async () => {
+    testDateTime = await testDateTimeMock.new();
+  });
 
+  it("(2018, 5, 24, 1, 2, 3) is a not a week end", async () => {
+      const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 24, 1, 2, 3);
+      (await testDateTime.isWeekEnd(timestamp)).should.be.false;
+  });
+
+  it("(2018, 5, 25, 1, 2, 3) is a not a week end", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 25, 1, 2, 3);
+    (await testDateTime.isWeekEnd(timestamp)).should.be.false;
+  });
+  
+  it("(2018, 5, 26, 1, 2, 3) is a week end", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 26, 1, 2, 3);
+    (await testDateTime.isWeekEnd(timestamp)).should.be.true;
+  });
+
+  it("(2018, 5, 27, 1, 2, 3) is a week end", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 27, 1, 2, 3);
+    (await testDateTime.isWeekEnd(timestamp)).should.be.true;
+  });
+});
+
+contract("---------- Test getDaysInMonth ---------- BokkyPooBahsDateTimeContract", async ([_, owner, wallet1, wallet2, wallet3, wallet4, wallet5]) => {
+  let testDateTime: TestDateTimeInstance;
+
+  beforeEach(async () => {
+    testDateTime = await testDateTimeMock.new();
+  });
+
+  it("(2000, 1, 24, 1, 2, 3) has 31 days", async () => {
+      const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 1, 24, 1, 2, 3);
+      (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+
+  it("(2000, 2, 24, 1, 2, 3) has 29 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 2, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(29);
+  });
+  
+  it("(2001, 2, 24, 1, 2, 3) has 28 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2001, 2, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(28);
+  });
+
+  it("(2000, 3, 24, 1, 2, 3) has 31 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 3, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+
+  it("(2000, 4, 24, 1, 2, 3) has 30 days", async () => {
+      const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 4, 24, 1, 2, 3);
+      (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(30);
+  });
+
+  it("(2000, 5, 24, 1, 2, 3) has 31 days", async () => {
+      const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 5, 24, 1, 2, 3);
+      (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+  
+  it("(2000, 6, 24, 1, 2, 3) has 30 days", async () => {
+      const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 6, 24, 1, 2, 3);
+      (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(30);
+  });
+
+  it("(2000, 7, 24, 1, 2, 3) has 31 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 7, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+
+  it("(2000, 8, 24, 1, 2, 3) has 31 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 8, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+
+  it("(2000, 9, 24, 1, 2, 3) has 30 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 9, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(30);
+  });
+
+  it("(2000, 10, 24, 1, 2, 3) has 31 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 10, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+
+  it("(2000, 11, 24, 1, 2, 3) has 30 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 11, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(30);
+  });
+
+  it("(2000, 12, 24, 1, 2, 3) has 31 days", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2000, 12, 24, 1, 2, 3);
+    (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
+  });
+});
