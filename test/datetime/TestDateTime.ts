2e5f75817dcf086328b6b250fb8765c0fe3f005b
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-11-05 12:57:00 +0300

    add test, get*, getDayOfWeek, _getDaysInMonth

--- test/datetime/TestDateTime.ts
+++ test/datetime/TestDateTime.ts
@@ -269,3 +269,130 @@ contract("---------- Test getDaysInMonth ---------- BokkyPooBahsDateTimeContract
     (await testDateTime.getDaysInMonth(timestamp)).toNumber().should.equal(31);
   });
 });
+
+contract("---------- Test _getDaysInMonth ---------- BokkyPooBahsDateTimeContract", async ([_, owner, wallet1, wallet2, wallet3, wallet4, wallet5]) => {
+  let testDateTime: TestDateTimeInstance;
+
+  beforeEach(async () => {
+    testDateTime = await testDateTimeMock.new();
+  });
+
+  it("2000/01 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 1)).toNumber().should.equal(31);
+  });
+
+  it("2000/02 has 29 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 2)).toNumber().should.equal(29);
+  });
+
+  it("2001/02 has 28 days", async () => {
+    (await testDateTime._getDaysInMonth(2001, 2)).toNumber().should.equal(28);
+  });
+
+  it("2000/03 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 3)).toNumber().should.equal(31);
+  });
+
+  it("2000/04 has 30 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 4)).toNumber().should.equal(30);
+  });
+
+  it("2000/05 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 5)).toNumber().should.equal(31);
+  });
+
+  it("2000/06 has 30 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 6)).toNumber().should.equal(30);
+  });
+
+  it("2000/07 has 31 day", async () => {
+    (await testDateTime._getDaysInMonth(2000, 7)).toNumber().should.equal(31);
+  });
+
+  it("2000/08 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 8)).toNumber().should.equal(31);
+  });
+
+  it("2000/09 has 30 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 9)).toNumber().should.equal(30);
+  });
+
+  it("2000/10 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 10)).toNumber().should.equal(31);
+  });
+
+  it("2000/11 has 30 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 11)).toNumber().should.equal(30);
+  });
+
+  it("2000/12 has 31 days", async () => {
+    (await testDateTime._getDaysInMonth(2000, 12)).toNumber().should.equal(31);
+  });
+
+});
+
+contract("--------- Test getDayOfWeek ---------- BokkyPooBahsDateTimeContract", async ([_, owner, wallet1, wallet2, wallet3, wallet4, wallet5]) => {
+  let testDateTime: TestDateTimeInstance;
+
+  beforeEach(async () => {
+    testDateTime = await testDateTimeMock.new();
+  });
+   
+  it("(2018, 5, 21, 1, 2, 3) is 1 Monday", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 21, 1, 2, 3);
+    (await testDateTime.getDayOfWeek(timestamp)).toNumber().should.equal(1);
+  });
+
+  it("(2018, 5, 24, 1, 2, 3) is 4 Thursday", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 24, 1, 2, 3);
+    (await testDateTime.getDayOfWeek(timestamp)).toNumber().should.equal(4);
+  });
+
+  it("(2018, 5, 26, 1, 2, 3) is 6 Saturday", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 26, 1, 2, 3);
+    (await testDateTime.getDayOfWeek(timestamp)).toNumber().should.equal(6);
+  });
+
+  it("(2018, 5, 27, 1, 2, 3) is 7 Sunday", async () => {
+    const timestamp: BigNumber = await testDateTime.timestampFromDateTime(2018, 5, 27, 1, 2, 3);
+    (await testDateTime.getDayOfWeek(timestamp)).toNumber().should.equal(7);
+  });
+
+});
+
+contract("---------- Test get* ---------- BokkyPooBahsDateTimeContract", async ([_, owner, wallet1, wallet2, wallet3, wallet4, wallet5]) => {
+  let testDateTime: TestDateTimeInstance;
+  let timestamp: BigNumber
+  beforeEach(async () => {
+    testDateTime = await testDateTimeMock.new();
+    timestamp = await testDateTime.timestampFromDateTime(2018, 5, 21, 1, 2, 3);
+  });
+   
+  it("(2018, 5, 21, 1, 2, 3)  - year is 2018", async () => {
+    
+    (await testDateTime.getYear(timestamp)).toNumber().should.equal(2018);
+  });
+
+  it("(2018, 5, 21, 1, 2, 3)  - month is 5 May", async () => {
+    (await testDateTime.getMonth(timestamp)).toNumber().should.equal(5);
+  });
+
+  it("(2018, 5, 21, 1, 2, 3)  - day is 21", async () => {
+    (await testDateTime.getDay(timestamp)).toNumber().should.equal(21);
+  });
+
+  it("(2018, 5, 21, 1, 2, 3)  - hour is 1", async () => {
+    
+    (await testDateTime.getHour(timestamp)).toNumber().should.equal(1);
+  });
+
+  it("(2018, 5, 21, 1, 2, 3)  - minute is 2", async () => {
+    
+    (await testDateTime.getMinute(timestamp)).toNumber().should.equal(2);
+  });
+
+  it("(2018, 5, 21, 1, 2, 3)  - second is 3", async () => {
+    
+    (await testDateTime.getSecond(timestamp)).toNumber().should.equal(3);
+  });
+});
